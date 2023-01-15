c033419c494ad41198f51ee8a54f07ff9fa83496
ReDay Zarra <redayzarra@gmail.com>
2023-01-15 18:57:02 -0500

    Understanding features

---
+++ Prerequisites.py
@@ -0,0 +1,10 @@
+# Terminology
+"""
+Features - are observations which can be:
+                      1. Continuous - prices
+                      2. Categorical - labels
+                      3. Ordinal - categories that can be ordered
+- Features are useful for predicting something of interest
+
+Labels -
+"""
