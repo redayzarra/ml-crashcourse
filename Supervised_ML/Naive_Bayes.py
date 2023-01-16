4af841f3869cdf08c15fbca73b5870afe63789ab
ReDay Zarra <redayzarra@gmail.com>
2023-01-15 22:55:25 -0500

    Understanding Naive Bayes

---
+++ Supervised_ML/Naive_Bayes.py
@@ -0,0 +1,6 @@
+"""
+Naive Bayes classifiers are essentially machine learning models that accept text as
+input and generate a probabiltity to classify the data. We could write it as:
+                  P(spam | message)
+The expression above basically means 
+"""
