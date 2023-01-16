6db0f7df88018d321f620c1f08e80840ee038635
ReDay Zarra <redayzarra@gmail.com>
2023-01-15 19:04:26 -0500

    Finishing prerequisites

--- Prerequisites.py
+++ Prerequisites.py
@@ -6,5 +6,25 @@ Features - are observations which can be:
                       3. Ordinal - categories that can be ordered
 - Features are useful for predicting something of interest
 
-Labels -
+Labels - are usually the dependent variable, or what we want to predict.
+"""
+
+# Math
+"""
+Arrays/Vectors
+
+Matrix - 2D arrays
+
+Polynomials
+
+Derivatives
+"""
+
+# Probability
+"""
+Basics
+
+Conditionals - how the situation changes, deck gets smaller after drawing a card
+
+Distributions - Gaussian(normal), Uniform (constant), Beta (conversion rates)
 """
